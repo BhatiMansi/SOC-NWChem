622fffe45eb0927dfa185a291eca1c3fff593c2e
Mansi Bhati <86735845+BhatiMansi@users.noreply.github.com>
2022-01-24 10:15:48 +0530

    Add files via upload

--- parse_nwchem.py
+++ parse_nwchem.py
@@ -1,6 +1,5 @@
 from scipy.io import FortranFile
 import numpy as np
-import re
 
 class parse_movecs:
     
@@ -19,15 +18,46 @@ class parse_movecs:
         self.nsets = f.read_record(dtype='int64') #No.of functions in each set
         self.nbf = f.read_record(dtype='int64') #No. of functions in basis 
         self.nmo = f.read_record(dtype='int64') #No. of vectors in each set
-        self.occ = f.read_record(dtype='float64') #Sequence of occ.-unocc. orbitals
-        self.ksenergies = f.read_record(dtype='float64')
-        mo_vecs = []
-        for i in range(self.nmo[0]):
-            mo_vecs.append(f.read_record(dtype='float64'))
-        self.mo_vecs = np.array(mo_vecs)
-        
-        f.close()  
+        if self.nsets == 1:
+            self.occ_unocc = f.read_record(dtype='float64') #Sequence of occ.-unocc. orbitals
+            occ = 0
+            for i in range(len(self.occ_unocc)):
+                if self.occ_unocc[i] == 2:
+                    occ +=1
+            self.occ = occ #Number of occupied states 
+            self.ksenergies = f.read_record(dtype='float64') #KS energies
+            mo_vecs = []
+            for i in range(self.nmo[0]):
+                mo_vecs.append(f.read_record(dtype='float64'))
+            self.mo_vecs = np.array(mo_vecs)
 
+        elif self.nsets == 2:
+            self.occ_unocc_alpha = f.read_record(dtype='float64') #Sequence of occ.-unocc. orbitals
+            occ_alpha = 0
+            for i in range(len(self.occ_unocc_alpha)):
+                if self.occ_unocc_alpha[i] == 1:
+                    occ_alpha +=1
+            self.occ_alpha = occ_alpha #Number of occupied alpha orbitals
+            self.ksenergies_alpha = f.read_record(dtype='float64') #KS alpha energies
+            mo_vecs_alpha = []
+            for i in range(self.nmo[0]):
+                mo_vecs_alpha.append(f.read_record(dtype='float64'))
+            self.mo_vecs_alpha = np.array(mo_vecs_alpha)
+
+            self.occ_unocc_beta = f.read_record(dtype='float64') #Sequence of occ.-unocc. orbitals
+            occ_beta = 0
+            for i in range(len(self.occ_unocc_beta)):
+                if self.occ_unocc_beta[i] == 1:
+                    occ_beta +=1
+            self.occ_beta = occ_beta #Number of occupied alpha orbitals
+            self.ksenergies_beta = f.read_record(dtype='float64') #KS beta energies
+            mo_vecs_beta = []
+            for i in range(self.nmo[1]):
+                mo_vecs_beta.append(f.read_record(dtype='float64'))
+            self.mo_vecs_beta = np.array(mo_vecs_beta)
+            
+        f.close()
+            
 class parse_civecs:
     
     def __init__(self, filename):
@@ -50,109 +80,56 @@ class parse_civecs:
         multiplicity = []
         XpY_vecs = []
         XmY_vecs = []
+        XpY_vecs_alpha = []
+        XmY_vecs_alpha = []
+        XpY_vecs_beta = []
+        XmY_vecs_beta = []
         
-        if self.tda == False:
-            for i in range(self.nroots[0]):
-                energies.append(f.read_record(dtype='float64'))
-                multiplicity.append(f.read_record(dtype='float64'))
-                XpY_vecs.append(f.read_record(dtype='float64'))
-                XmY_vecs.append(f.read_record(dtype='float64'))
-            self.energies = energies
-            self.multiplicity = multiplicity
-            self.XpY_vecs = np.array(XpY_vecs)
-            self.XmY_vecs = np.array(XmY_vecs)
+        if self.ipol == 1:
+            if self.tda == False:
+                for i in range(self.nroots[0]):
+                    energies.append(f.read_record(dtype='float64')[0])
+                    multiplicity.append(f.read_record(dtype='float64')[0])
+                    XpY_vecs.append(f.read_record(dtype='float64'))
+                    XmY_vecs.append(f.read_record(dtype='float64'))
+                self.energies = energies
+                self.multiplicity = multiplicity
+                self.XpY_vecs = np.array(XpY_vecs)
+                self.XmY_vecs = np.array(XmY_vecs)
 
-        else:
-            for i in range(self.nroots[0]):
-                energies.append(f.read_record(dtype='float64'))
-                multiplicity.append(f.read_record(dtype='float64'))
-                XpY_vecs.append(f.read_record(dtype='float64'))
-            self.energies = energies
-            self.multiplicity = multiplicity
-            self.XpY_vecs = np.array(XpY_vecs)
+            else:
+                for i in range(self.nroots[0]):
+                    energies.append(f.read_record(dtype='float64')[0])
+                    multiplicity.append(f.read_record(dtype='float64')[0])
+                    XpY_vecs.append(f.read_record(dtype='float64'))
+                self.energies = energies
+                self.multiplicity = multiplicity
+                self.XpY_vecs = np.array(XpY_vecs)
+                
+        if self.ipol == 2:
+            if self.tda == False:
+                for i in range(self.nroots[0]):
+                    energies.append(f.read_record(dtype='float64')[0])
+                    multiplicity.append(f.read_record(dtype='float64')[0])
+                    XpY_vecs_alpha.append(f.read_record(dtype='float64'))
+                    XmY_vecs_alpha.append(f.read_record(dtype='float64'))
+                    XpY_vecs_beta.append(f.read_record(dtype='float64'))
+                    XmY_vecs_beta.append(f.read_record(dtype='float64'))
+                self.energies = energies
+                self.multiplicity = multiplicity
+                self.XpY_vecs_alpha = np.array(XpY_vecs_alpha)
+                self.XmY_vecs_alpha = np.array(XmY_vecs_alpha)
+                self.XpY_vecs_beta = np.array(XpY_vecs_beta)
+                self.XmY_vecs_beta = np.array(XmY_vecs_beta)
+
+            else:
+                for i in range(self.nroots[0]):
+                    energies.append(f.read_record(dtype='float64')[0])
+                    multiplicity.append(f.read_record(dtype='float64')[0])
+                    XpY_vecs.append(f.read_record(dtype='float64'))
+                self.energies = energies
+                self.multiplicity = multiplicity
+                self.XpY_vecs_alpha = np.array(XpY_vecs_alpha)
+                self.XpY_vecs_beta = np.array(XpY_vecs_beta)
         
         f.close()
-        
-
-def read_soc(fname):
-
-    basis_dict,dft_dict,tddft_dict,soc_dict = [{} for _ in range(4)]
-    filetxt = open(fname,'r').read()
-
-    ## reading a geometry block
-    geom_pattern = re.compile(r'^[ \t]*geometry[ \t\S]*\n'
-                                r'((?:^[ \t]*[\S]+[ \t\S]*\n)+)'
-                                r'^[ \t]*end\n\n',re.M|re.IGNORECASE)
-
-    for match in geom_pattern.findall(filetxt):
-        geomstr = match
-
-    ## Charge of the molecule
-    charge_pattern = re.compile("charge -*\+*[0-9]+",re.IGNORECASE)
-    charge = 0.0
-    for match in charge_pattern.findall(filetxt):
-        charge = float(match.rsplit()[-1])
-
-    ## reading basis block
-    basis_pattern = re.compile(r'^[ \t]*basis\n'
-                                r'((?:^[ \t]*[\S]+[ \t\S]*\n)+)'
-                                r'^[ \t]*end\n\n',re.M|re.IGNORECASE)
-
-    for match in basis_pattern.findall(filetxt):
-        match = match.lower()
-        if '* library' in match:
-            basis_dict['*'] = match.split()[2]
-        else:
-            for string in match.splitlines():
-                basis_dict[string.split()[0]] = string.split()[1]
-
-    ## reading dft block
-    dft_pattern = re.compile(r'^[ \t]*DFT\n'
-                            r'((?:^[ \t]*[\S]+[ \t\S]*\n)+)'
-                            r'^[ \t]*end\n\n',re.M|re.IGNORECASE)
-
-    for match in dft_pattern.findall(filetxt):
-        match = match.lower()
-        for string in match.splitlines():
-            try:
-                dft_dict[string.split()[0]] = string.split()[1]
-            except IndexError:
-                dft_dict[string.split()[0]] = None
-
-    ## reading tddft block
-    tddft_pattern = re.compile(r'^[ \t]*TDDFT\n'
-                            r'((?:^[ \t]*[\S]+[ \t\S]*\n)+)'
-                            r'^[ \t]*end\n\n',re.M|re.IGNORECASE)
-
-    for match in tddft_pattern.findall(filetxt):
-        match = match.lower()
-        for string in match.splitlines():
-            try:
-                tddft_dict[string.split()[0]] = string.split()[1]
-            except IndexError:
-                tddft_dict[string.split()[0]] = None
-
-    ## reading soc block
-    soc_pattern = re.compile(r'^[ \t]*soc\n'
-                            r'((?:^[ \t]*[\S]+[ \t\S]*\n)+)'
-                            r'^[ \t]*end\n\n',re.M|re.IGNORECASE)
-
-    for match in soc_pattern.findall(filetxt):
-        match = match.lower()
-        for string in match.splitlines():
-            try:
-                soc_dict[string.split()[0]] = string.split()[1]
-            except IndexError:
-                soc_dict[string.split()[0]] = None
-
-    master_dict = {'geom_str' : geomstr,
-            'charge' : charge,
-            'basis_dict' : basis_dict,
-            'dft_dict' : dft_dict,
-            'tddft_dict' : tddft_dict,
-            'soc_dict' : soc_dict,
-            }
-
-    return master_dict        
-        
-   
